634f60c72a732916783aeb338de77c37be17b1bd
Joshua <joshsmayhew@gmail.com>
2022-11-15 15:09:31 +0700

    commit tree d8b677c7c0919a80c986c6735f28750885a1494e

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(' and ')
+  else
+    array_end = ", and #{array.pop}"
+    array.join(', ') << array_end
+
+  end
 end
+
+oxford_comma(%w[fiddleheads okra kohlrabi])
